e4ae1af089d9618938fb06af2a9f4552d10098cc
cesarluilly <cesarluilly.garcia@hotmail.com>
2024-08-09 06:03:42 -0600

    Video 136 Refactorizaciones en nuestro servicio

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map, Observable, of, tap } from 'rxjs';
+import { catchError, delay, map, Observable, of, tap } from 'rxjs';
 import { ICountry } from '../interfaces/ICountry';
 
 
@@ -16,35 +16,7 @@ export class CountriesService {
 
   }
 
-
-  searchCountryByAlphaCode(code: string): Observable<ICountry | null>{
-    const url = `${this.apiURL}/alpha/${code}`;
-
-    return this.httpClient.get<ICountry[]>(url)
-    .pipe(
-      map(countries => countries.length > 0 ? countries[0] : null),
-      catchError(error => {
-        console.log(error);
-
-        return of(null)
-      })
-    );
-  }
-
-
-  searchCapital(term: string): Observable<ICountry[]>{
-    //  Como vemos, devuelve un observable, pero para que se
-    //    ejecute, tenemos que suscribirnos al observable.
-
-    const url = `${this.apiURL}/capital/${term}`;
-
-    // Podemos hacer uso de los operadores RxJS
-    // return this.httpClient.get<ICountry[]>(url)
-    // .pipe(
-    //   tap(item => console.log('Tab1', item)),
-    //   map(item => []),
-    //   tap(item => console.log('Tab2', item)),
-    // );
+  private getCountriesRequest(url: string): Observable<ICountry[]> {
 
     return this.httpClient.get<ICountry[]>(url)
     // Agregamos esta linea, para en caso de que el api mande error, entonces que por defecto
@@ -59,29 +31,51 @@ export class CountriesService {
     );
   }
 
-  searchCountry(term: string): Observable<ICountry[]>{
-    const url = `${this.apiURL}/name/${term}`;
+  searchCountryByAlphaCode(code: string): Observable<ICountry | null>{
+    const url = `${this.apiURL}/alpha/${code}`;
 
     return this.httpClient.get<ICountry[]>(url)
     .pipe(
+      map(countries => countries.length > 0 ? countries[0] : null),
       catchError(error => {
         console.log(error);
 
-        return of([])
-      })
+        return of(null)
+      }),
+      // Lo utilizamos para simular que el servicio esta
+      //    tardando un poner un loader.
+      delay ( 2000 )
     );
+  }
+
+
+  searchCapital(term: string): Observable<ICountry[]>{
+    //  Como vemos, devuelve un observable, pero para que se
+    //    ejecute, tenemos que suscribirnos al observable.
+
+    const url = `${this.apiURL}/capital/${term}`;
+
+    return this.getCountriesRequest(url);
+
+    // Podemos hacer uso de los operadores RxJS
+    // return this.httpClient.get<ICountry[]>(url)
+    // .pipe(
+    //   tap(item => console.log('Tab1', item)),
+    //   map(item => []),
+    //   tap(item => console.log('Tab2', item)),
+    // );
+
+  }
+
+  searchCountry(term: string): Observable<ICountry[]>{
+    const url = `${this.apiURL}/name/${term}`;
+
+    return this.getCountriesRequest(url);
   }
 
   searchCountryByRegion(term: string): Observable<ICountry[]>{
     const url = `${this.apiURL}/region/${term}`;
 
-    return this.httpClient.get<ICountry[]>(url)
-    .pipe(
-      catchError(error => {
-        console.log(error);
-
-        return of([])
-      })
-    );
+    return this.getCountriesRequest(url);
   }
 }

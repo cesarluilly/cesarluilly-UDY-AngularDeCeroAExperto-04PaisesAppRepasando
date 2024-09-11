3cc4b79076c42487003235b96a7b6467c5a05deb
cesarluilly <cesarluilly.garcia@hotmail.com>
2024-09-10 21:52:22 -0600

    video 140 limpieza de suscripciones

--- src/app/shared/components/searchBox/searchBox.component.ts
+++ src/app/shared/components/searchBox/searchBox.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { debounceTime, Subject } from 'rxjs';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { debounceTime, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
@@ -10,13 +10,15 @@ import { debounceTime, Subject } from 'rxjs';
     }
   `]
 })
-export class SearchBoxComponent implements OnInit{
-
+export class SearchBoxComponent implements OnInit, OnDestroy{
   //  Tubo de Agua
   //  Este termino es para cuando tecleamos en un Input, pero al momento de
   //    dejar de teclar, entonces queremos que se lance una peticion al
   //    back.
   private debouncer: Subject<string> = new Subject<string>();
+
+  // De esta forma manejamos de forma independiente la suscripcion.
+  private debouncerSescription? : Subscription;
 
   @Input()
   public placeholdercesar: string = ''
@@ -30,17 +32,30 @@ export class SearchBoxComponent implements OnInit{
   ngOnInit(): void{
 
     //  Encargado de hacer las emisiones es este.
-    this.debouncer
+    this.debouncerSescription = this.debouncer
     .pipe(
       //  Me tengo que esperar un segundo para ver si no
       //    recibo mas valores y no emito nada.
       debounceTime(300)
     )
+    // Siempre que tengas un suscribe, hay que hacer limp
     .subscribe(value => {
       console.log('debouncer value', value);
 
       this.OnDebounce.emit(value);
     })
+  }
+
+  ngOnDestroy(): void {
+    console.log('Destruido');
+
+    // Siempre que hagamos un suscribe, hay que limpiarlo despues
+    //    de que ya no lo necesitamos, ya que de lo contrario
+    //    la suscripcion va a estar escuchando a pesar de ya no
+    //    tener un componente que ya no lo necesite.
+    // Esto nos ayudar a tener la memoria de los observable
+    //   al minimo.
+    this.debouncerSescription?.unsubscribe();
   }
 
   emitValue(value:string): void{
